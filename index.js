66b8591ad630c5d46175b62a45e9925b311d0ee1
muhammad Ali <ma0637579@gmail.com>
2024-04-12 22:06:40 +0500

    push my Currency converter to github

---
+++ index.js
@@ -0,0 +1,40 @@
+//import inquirer;
+//object;
+//user input;
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    PKR: 277.98,
+    GBP: 0.79,
+    EUR: 0.92,
+    CAD: 1.36,
+    AED: 3.67,
+    AUD: 1.51,
+    SAR: 3.75,
+};
+let userAnswer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "enter from currency",
+        type: "list",
+        choices: ["USD", "PKR", "GBP", "EUR", "CAD", "AED", "AUD", "SAR"]
+    },
+    {
+        name: "to",
+        message: "enter to currency",
+        type: "list",
+        choices: ["USD", "PKR", "GBP", "EUR", "CAD", "AED", "AUD", "SAR"]
+    },
+    {
+        name: "amount",
+        message: "enter your amount",
+        type: "number",
+    }
+]);
+let fromAmount = currency[userAnswer.from];
+let toAmount = currency[userAnswer.to];
+let Amount = userAnswer.amount;
+let baseAmount = Amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+let roundedconvertedAmount = parseInt(convertedAmount);
+console.log(roundedconvertedAmount);
---
+++ index.ts
@@ -0,0 +1,52 @@
+#! /usr/bin/env node
+
+//import inquirer;
+//object;
+//user input;
+
+import inquirer from "inquirer"; 
+
+const currency:any = {
+
+    USD: 1,       //BASE CURRENCY
+    PKR: 277.98,    
+    GBP: 0.79,
+    EUR: 0.92,
+    CAD: 1.36,
+    AED: 3.67,
+    AUD: 1.51,
+    SAR: 3.75,
+
+}
+let userAnswer = await inquirer.prompt(
+ [
+    {
+         name:"from",
+         message: "enter from currency",
+         type:"list",
+         choices:["USD","PKR","GBP","EUR","CAD","AED","AUD","SAR"]
+    },
+    {
+        name: "to",
+        message:"enter to currency",
+        type:"list",
+        choices:["USD","PKR","GBP","EUR","CAD","AED","AUD","SAR"]
+    },
+    {
+        name:"amount",
+        message: "enter your amount",
+        type: "number",
+    }
+])
+let fromAmount = currency[userAnswer.from];
+let toAmount = currency[userAnswer.to];
+let Amount = userAnswer.amount;
+let baseAmount = Amount / fromAmount;
+let convertedAmount:any = baseAmount * toAmount; 
+
+let roundedconvertedAmount = parseInt(convertedAmount);
+console.log(roundedconvertedAmount);
+
+
+
+
